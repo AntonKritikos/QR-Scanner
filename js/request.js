a60fd59c6d2d0eaefbe8d4efce0b1d30705947a8
Anton <a.kritikos1997@gmail.com>
2017-10-18 13:05:27 +0200

    New Qr-reader

--- js/request.js
+++ js/request.js
@@ -4,6 +4,3 @@ function Get(yourUrl){
     Httpreq.send(null);
     return Httpreq.responseText;
 }
-var json_obj = JSON.parse(Get("https://jxdemoserver.intershop.de/INTERSHOP/rest/WFS/inSPIRED-inTRONICS_Business-Site/-/products"));
-console.log(json_obj);
-// /ECO-50/ECO-50200
