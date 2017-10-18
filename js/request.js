864521eedb9d7dbb1ac86a2d2382bc13b1b40fa0
Anton <a.kritikos1997@gmail.com>
2017-10-18 13:20:19 +0200

    Qr wekend

--- js/request.js
+++
@@ -1,6 +0,0 @@
-function Get(yourUrl){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
-}
