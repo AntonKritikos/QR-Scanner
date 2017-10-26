3bb18bc262f0654f58cc1b578408e54c9a26473b
Anton <a.kritikos1997@gmail.com>
2017-10-26 11:43:38 +0200

    Change

--- js/master.js
+++ js/master.js
@@ -61,15 +61,30 @@ var vue = new Vue({
     },
     getImage (data){
       return "https://demoimages.sellsmart.nl/Sellsmart-B2XDefault-Site/images/XS/"+data+".jpg";
-    },
-    truncate: function(string, value) {
-    	return string.substring(0, value) + '...';
     }
   },
   filters: {
 
-      truncate: function(string, value) {
-      return string.substring(0, value) + '...';
+    limitWords(textToLimit, wordLimit)
+    {
+    var finalText = "";
+
+    var text2 = textToLimit.replace(/\s+/g, ' ');
+
+    var text3 = text2.split(' ');
+
+    var numberOfWords = text3.length;
+
+    var i=0;
+
+    if(numberOfWords > wordLimit)
+    {
+    for(i=0; i< wordLimit; i++)
+    finalText = finalText+" "+ text3[i]+" ";
+
+    return finalText+"...";
+    }
+    else return textToLimit;
     }
   }
 });
